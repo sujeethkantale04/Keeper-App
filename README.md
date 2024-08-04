8f2043e05a0c1d83bb0e46e2a6cca933fdd1cd86
Kantale Sujeeth <104816459+sujeethkantale04@users.noreply.github.com>
2024-08-05 00:47:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Keeper App
+Keeper App is a web-based note-taking application with an interactive UI that allows users to add, delete, and manage their notes efficiently
+
+### Features
+- Add Notes: Create new notes with a title and content.
+- Delete Notes: Remove notes when they are no longer needed.
+
+### Technologies Used:
+- React
+- HTML
+- CSS
+
+### Getting Started:
+1. Clone the Repository
+2. cd to the folder of the project "Keeper App"
+3. run npm install to install all the dependencies
+6. npm run dev to initialize the page
+7. Your app is live and can be running on localhost:5173/
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,51 @@
+import React,{ useState } from "react";
+
+function CreateArea(props) {
+    const [note,setNote] = useState({
+        title : "",
+        content : ""
+    })
+
+    function handleChange(event){
+        const {name,value} = event.target;
+        // console.log(event.target);
+        setNote((prevNote)=>{
+            return {
+                ...prevNote,
+                [name] : value
+            }
+        })
+    }
+    function submitNote(event){
+        props.onAdd(note)
+
+        event.preventDefault();
+        setNote({
+            title : "",
+            content : ""
+        })
+    }
+
+  return (
+    <div>
+      <form>
+        <input 
+            name="title" 
+            value={note.title} 
+            placeholder="Title" 
+            onChange={handleChange}
+        />
+        <textarea 
+            name="content" 
+            value={note.content} 
+            placeholder="Take a note..." 
+            rows="3" 
+            onChange={handleChange}
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
